3abe5d4386c99b046b8a98f9f49e4ce088748e1a
Khapham <khar22102003@gmail.com>
2024-07-24 08:46:23 +0700

    Create custom User Registration Form

--- Commerceshop/urls.py
+++ Commerceshop/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("core.durians"))
+    path("", include("core.durians")),
+    path("user/", include("userauths.urls"))
 ]
 
 if settings.DEBUG:
--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from userauths.forms import UserRegisterForm
 
 # Create your views here.
+def register_view(request):
+
+    if request.method == 'POST':
+        pass
+    form = UserRegisterForm()
+    context = {
+        'form': form
+
+    }
+    return render(request, "userauths/sign-up.html", context=context)
