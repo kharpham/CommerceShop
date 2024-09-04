7d06e5ddbc48d77ca6260af7c3ee8263734e3cdc
Khapham <khar22102003@gmail.com>
2024-09-04 16:31:40 +0700

    Add front-end price filter

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,23 +1,25 @@
-from core.models import Category, Address, Vendor
+from core.models import Category, Address, Vendor, Product
+from django.db.models import Min, Max
 
 def default(request):
     categories = Category.objects.all()
     vendors = Vendor.objects.all()
-    if not request.user.is_authenticated:
-        return {
-            'categories': categories,
-            'vendors': vendors,
-        } 
+    min_max_price = Product.objects.aggregate(Min("price"), Max("price"))
+
+
+    # if not request.user.is_authenticated:
+    #     return {
+    #         'categories': categories,
+    #         'vendors': vendors,
+    #     } 
     try:
         address = Address.objects.get(user=request.user)
-        return {
+    except:
+        address = None
+    return {
         'categories': categories,
-        'address': address,
         'vendors': vendors,
-    }
-    except Address.DoesNotExist:
-        return {
-            'categories': categories,
-            'vendors': vendors,
-        } 
+        'address': address,
+        'min_max_price': min_max_price,
+    } 
     
