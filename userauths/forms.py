f84c161fe4b11cc785b561012c579f1189948d94
Khapham <khar22102003@gmail.com>
2024-09-15 19:56:20 +0700

    Add edit user profile functionality

--- userauths/forms.py
+++ userauths/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from userauths.models import User
+from userauths.models import User, Profile
 
 class UserRegisterForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Username"}))
@@ -10,3 +10,12 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', ]
+
+class ProfileUpdateForm(forms.ModelForm):
+    full_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Full Name"}))
+    bio = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Bio"}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Mobile Phone"}))
+    image = forms.ImageField()
+    class Meta:
+        model = Profile
+        fields = ['full_name', 'bio', 'phone', 'image']
--- userauths/views.py
+++ userauths/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from userauths.forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from userauths.forms import UserRegisterForm, ProfileUpdateForm
 from django.contrib import messages
 from django.conf import settings
 from userauths.models import User
@@ -49,3 +50,22 @@ def logout_view(request):
     logout(request)
     messages.success(request, "You are logged out.")
     return redirect("userauths:sign-in")
+
+@login_required()
+def profile_update(request):
+    profile = request.user.profile
+    if request.method == "POST":
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            messages.success(request, "Profile updated successfully.")
+            return redirect("core:dashboard")
+    print(profile)
+    form = ProfileUpdateForm(instance=profile)
+    context = {
+        "form": form,
+        "profile": profile,
+    }
+    return render(request, "userauths/profile-update.html", context)
